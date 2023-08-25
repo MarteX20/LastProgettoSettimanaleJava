fc4cc023d4c46d457623079b6f953a90b532d0aa
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-08-25 14:06:30 +0200

    implementazione FutureCommunicationChannel e HttpCommunicationChannel

--- src/main/java/ProgettoSettimanaleLast/prog/FutureCommunicationChannel.java
+++ src/main/java/ProgettoSettimanaleLast/prog/FutureCommunicationChannel.java
@@ -1,4 +1,7 @@
 package ProgettoSettimanaleLast.prog;
 
-public class FutureCommunicationChannel {
+public class FutureCommunicationChannel implements CommunicationChannel {
+    @Override
+    public void sendAlert(int idSonda, double latitude, double longitude, int smokeLevel) {
+    }
 }
--- src/main/java/ProgettoSettimanaleLast/prog/HttpCommunicationChannel.java
+++ src/main/java/ProgettoSettimanaleLast/prog/HttpCommunicationChannel.java
@@ -1,4 +1,9 @@
 package ProgettoSettimanaleLast.prog;
 
-public class HttpCommunicationChannel{
+public class HttpCommunicationChannel implements CommunicationChannel {
+    @Override
+    public void sendAlert(int idSonda, double latitude, double longitude, int smokeLevel) {
+        String url = "http://host/alarm?idsonda=" + idSonda + "&lat=" + latitude + "&lon=" + longitude + "&smokelevel=" + smokeLevel;
+        System.out.println("Alert sent via HTTP: " + url);
+    }
 }
